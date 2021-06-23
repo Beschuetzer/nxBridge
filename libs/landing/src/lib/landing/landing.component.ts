0698e51ce83a8b245fa31f8b7884afec4ac79c98
Adam <adam.j.major@gmail.com>
2021-06-23 14:29:55 -0700

    add setError(); initial getUser request works

--- libs/landing/src/lib/landing/landing.component.ts
+++ libs/landing/src/lib/landing/landing.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LandingService } from '../landing.service';
 import { User } from '@nx-bridge/interfaces-and-types';
+import { exhaustMap, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'nx-bridge-landing',
@@ -12,28 +14,28 @@ import { User } from '@nx-bridge/interfaces-and-types';
 export class LandingComponent implements OnInit {
   constructor(
     private landingService: LandingService,
-    private http: HttpClient,
+    private http: HttpClient
   ) {}
 
+  public error = '';
   public initialForm: FormGroup = new FormGroup({});
-
 
   get emailIsValid(): boolean | undefined {
     const email = this.initialForm.get('email');
     if (!email) return true;
-    return email.touched && email.value !== '' && email.valid
+    return email.touched && email.value !== '' && email.valid;
   }
 
   get usernameIsValid(): boolean | undefined {
     const username = this.initialForm.get('username');
     if (!username) return true;
-    return username.touched && username.value !== '' && username.valid
+    return username.touched && username.value !== '' && username.valid;
   }
 
   get stringToSearchUsing(): string | undefined {
     const preStringUsername = 'Search for user with username of ';
     const preStringEmail = 'Search for user with email of ';
-    
+
     const email = this.initialForm.get('email')?.value;
     const username = this.initialForm.get('username')?.value;
 
@@ -46,8 +48,6 @@ export class LandingComponent implements OnInit {
     else return usernameString;
   }
 
-  
-
   ngOnInit(): void {
     this.initializeForm();
   }
@@ -57,27 +57,24 @@ export class LandingComponent implements OnInit {
   }
 
   private initializeForm() {
-    this.initialForm = new FormGroup({
-      "username": new FormControl(
-        null,
-        [
+    this.initialForm = new FormGroup(
+      {
+        username: new FormControl(null, [
           Validators.maxLength(12),
-          LandingService.noEmpty
-        ]
-      ),
-      "email": new FormControl(
-        null,
-        [
+          LandingService.noEmpty,
+        ]),
+        email: new FormControl(null, [
           Validators.email,
-          LandingService.noEmpty
-        ]
-      )
-    }, LandingService.numberRequired(1));
+          LandingService.noEmpty,
+        ]),
+      },
+      LandingService.numberRequired(1)
+    );
   }
 
   // private validateRequired (formGroup: FormGroup) {
   //   if ((formGroup?.get('email') as FormGroup)?.value !== '' || (formGroup?.get('username') as FormGroup)?.value !== '') return {formValid: true};
-  
+
   //   return null;
   // }
 
@@ -88,8 +85,9 @@ export class LandingComponent implements OnInit {
     const usernameValue = username?.value;
 
     const userInLocalStorage = localStorage.getItem('user');
-    const parsed = userInLocalStorage ? JSON.parse(userInLocalStorage) as User : null;
-    //todo: need to check if the userInLocalStorage data matches what is being search and the userInLocalStorage._id is valid, if so skip below 
+    const parsed = userInLocalStorage
+      ? (JSON.parse(userInLocalStorage) as User)
+      : null;
 
     console.log('email =', emailValue);
     console.log('username =', usernameValue);
@@ -97,16 +95,40 @@ export class LandingComponent implements OnInit {
     console.log('parsed =', parsed);
 
     if (!parsed?._id || parsed?.username !== usernameValue) {
-      console.log('inside if------------------------------------------------');
-      this.http.post<User>('/api/getUser', { email: emailValue, username: usernameValue}).subscribe(user => {
-        console.log('user =', user);
-        if (user) localStorage.setItem('user', JSON.stringify({...user, email: null, salt: null, hash: null, resetPasswordExpires: null, resetPasswordToken: null} as User));
-        else {
-          localStorage.removeItem('user');
-          //todo: need to display some message to user that no match found
-        }
-      })
+      this.http
+        .post<User>('/api/getUser', {
+          email: emailValue,
+          username: usernameValue,
+        })
+        .subscribe((user) => {
+          console.log('user =', user);
+
+          if (user)
+            localStorage.setItem(
+              'user',
+              JSON.stringify({
+                ...user,
+                email: null,
+                salt: null,
+                hash: null,
+                resetPasswordExpires: null,
+                resetPasswordToken: null,
+              } as User)
+            );
+          else {
+            localStorage.removeItem('user');
+            //TODO: what happens here?
+            this.setError(
+              `There is no user with the ${
+                this.usernameIsValid ? 'username' : 'email'
+              } of ${this.usernameIsValid ? usernameValue : emailValue}.`
+            );
+          }
+        });
     }
   }
-  
+
+  private setError(error: string) {
+    this.error = error;
+  }
 }
