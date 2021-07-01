307c021504ffb7f6d585f33da241752508d4037e
Adam <adam.j.major@gmail.com>
2021-06-30 21:12:08 -0700

    Setup paperjs

--- libs/deal-player/src/lib/deal-player/deal-player.component.ts
+++ libs/deal-player/src/lib/deal-player/deal-player.component.ts
@@ -1,37 +1,59 @@
-
 import { Component, HostBinding, OnInit } from '@angular/core';
-import { DEAL_PLAYER_CLASSNAME, VISIBLE_CLASSNAME } from '@nx-bridge/constants';
+import {
+  DEAL_PLAYER_CLASSNAME,
+  VISIBLE_CLASSNAME,
+  DISPLAY_NONE_CLASSNAME,
+} from '@nx-bridge/constants';
+
 import { Store } from '@ngrx/store';
 import { AppState } from '@nx-bridge/store';
 import { Deal } from '@nx-bridge/interfaces-and-types';
-import { debug } from 'node:console';
+import * as paper from 'paper';
+import { Path, Project, Raster } from 'paper/dist/paper-core';
 
 @Component({
   selector: 'nx-bridge-deal-player',
   templateUrl: './deal-player.component.html',
-  styleUrls: ['./deal-player.component.scss']
+  styleUrls: ['./deal-player.component.scss'],
 })
 export class DealPlayerComponent implements OnInit {
-  @HostBinding(`class.${DEAL_PLAYER_CLASSNAME}`) get classname() {return true}
+  @HostBinding(`class.${DEAL_PLAYER_CLASSNAME}`) get classname() {
+    return true;
+  }
   public DEAL_PLAYER_CLASSNAME = DEAL_PLAYER_CLASSNAME;
+  public DISPLAY_NONE_CLASSNAME = DISPLAY_NONE_CLASSNAME;
   public deal: Deal | null = null;
   public isPlaying = false;
+  public scope: paper.PaperScope | null = null;
+  public project: paper.Project | null = null;
+  public isMobile = window.innerWidth <= 655;
 
-  constructor(
-    private store: Store<AppState>,
-  ) { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store.select('deals').subscribe(dealState => {
+    this.store.select('deals').subscribe((dealState) => {
+      console.log('dealState =', dealState);
       this.deal = dealState.currentlyViewingDeal;
-      console.log('this.deal =', this.deal);
-    })
+      this.updateCanvas();
+    });
   }
 
   onClose() {
-    document.querySelector(`.${DEAL_PLAYER_CLASSNAME}`)?.classList.remove(VISIBLE_CLASSNAME)
+  document
+      .querySelector(`.${DEAL_PLAYER_CLASSNAME}`)
+      ?.classList.remove(VISIBLE_CLASSNAME);
   }
 
+  private updateCanvas() {
+      debugger;
+      window['paper'] = paper;
+    this.scope = new paper.PaperScope();
+    const project1 = new Project(document.querySelector('#cv1') as HTMLCanvasElement);
+    const path = new Path.Circle({
+      center: [80, 50],
+      radius: 30,
+      strokeColor: 'black'
+    });
+    
+  }
 }
-
-	
