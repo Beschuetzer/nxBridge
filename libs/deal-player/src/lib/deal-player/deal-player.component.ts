8b5999ced71cf96faf2b5a08857950e2362d9602
Adam <adam.j.major@gmail.com>
2021-06-30 23:34:46 -0700

    Add outline to renderHand()"

--- libs/deal-player/src/lib/deal-player/deal-player.component.ts
+++ libs/deal-player/src/lib/deal-player/deal-player.component.ts
@@ -4,11 +4,13 @@ import {
   VISIBLE_CLASSNAME,
   DISPLAY_NONE_CLASSNAME,
   flatten,
+  getDirectionFromSeating,
+  cardinalDirections,
 } from '@nx-bridge/constants';
 
 import { Store } from '@ngrx/store';
 import { AppState } from '@nx-bridge/store';
-import { Deal, Seating } from '@nx-bridge/interfaces-and-types';
+import { Deal, Hand, Seating } from '@nx-bridge/interfaces-and-types';
 import * as paper from 'paper';
 import { Path, Project, Raster } from 'paper/dist/paper-core';
 
@@ -33,6 +35,7 @@ export class DealPlayerComponent implements OnInit {
   private cardsLoaded = 0;
   private cardWidth = -1;
   private cardSpacingIncrement = -1;
+  private defaultCardPosition = -1000;
 
   constructor(private store: Store<AppState>) {}
 
@@ -40,7 +43,6 @@ export class DealPlayerComponent implements OnInit {
     this.store.select('games').subscribe((gameState) => {
       this.seating = gameState.currentlyViewingGameSeating;
     })
-    
 
     this.store.select('deals').subscribe((dealState) => {
       this.deal = dealState.currentlyViewingDeal;
@@ -68,7 +70,7 @@ export class DealPlayerComponent implements OnInit {
   private loadCards() {
     for (let i = 0; i < 52; i++) {
       const newRaster = new Raster(`card-${i}`);
-      newRaster.position.x = -1000;
+      newRaster.position.x = this.defaultCardPosition;
       newRaster.scale(.5);
       newRaster.onLoad = this.onCardLoad.bind(this);
       this.cards.push(newRaster);
@@ -78,6 +80,7 @@ export class DealPlayerComponent implements OnInit {
   private onCardLoad() {
     this.cardsLoaded += 1;
     if (this.cardsLoaded >= 52) {
+      this.resetAllCardPositions();
       this.renderHands();
     }
   }
@@ -86,25 +89,58 @@ export class DealPlayerComponent implements OnInit {
     for (const username in this.deal?.hands) {
       if (Object.prototype.hasOwnProperty.call(this.deal?.hands, username)) {
         const usersHand = this.deal?.hands[username];
-        //todo: need to get the seating from currentlyViewingGame
-        
+        const usersDirection = getDirectionFromSeating(this.seating as Seating, username);
+        this.renderHand(usersHand as Hand, usersDirection);
       }
     }
   }
 
-  private renderHand(hand: [number[]], position: string) {
+  private renderHand(hand: Hand, direction: string) {
     const flatHand = flatten(hand);
-    this.cardWidth = (flatHand[0] as paper.Raster).bounds.width;
+    this.cardWidth = (this.cards[0] as paper.Raster).bounds.width;
     this.cardSpacingIncrement = this.cardWidth / 6;
     const startingPositionX = this.getStartingPositionX(flatHand.length);
 
-    for (let i = 0; i < 13; i++) {
-      (this.cards[i] as paper.Raster).position.y = 500;
-      (this.cards[i] as paper.Raster).position.x = startingPositionX + this.cardWidth / 2 + this.cardSpacingIncrement * i;
+    for (let i = 0; i < flatHand.length; i++) {
+      const cardAsNumber = flatHand[i];
+      const cardAsRaster = this.cards.find((card: paper.Raster) => {
+        const indexOfDash = card.image.id.indexOf('-');
+        const digits = card.image.id.slice(indexOfDash + 1);
+        return(digits).match(cardAsNumber);
+      });
+
+      this.setXPositionOfHands(cardAsRaster, i, direction, startingPositionX);
+      this.setYPositionOfHands(cardAsRaster, i, direction, startingPositionX)
+      this.project?.activeLayer.addChild(cardAsRaster);
     }
   }
 
+  private setXPositionOfHands(cardAsRaster: paper.Raster, nthCard: number, direction: string, startingPositionX: number) {
+    if (direction === cardinalDirections[0] || direction === cardinalDirections[2]) {
+      cardAsRaster.position.x = startingPositionX + this.cardWidth / 2 + this.cardSpacingIncrement * nthCard;
+    }
+
+    else if (direction === cardinalDirections[1]) {
+      
+    }
+
+    else if (direction === cardinalDirections[3]) {
+      
+    }
+  }
+
+  private setYPositionOfHands(cardAsRaster: paper.Raster, nthCard: number, direction: string, startingPositionY: number) {
+    cardAsRaster.position.y = 500;
+  }
+
   private getStartingPositionX(lengthOfHand: number) {
-    return (window.innerWidth - (this.cardWidth + (lengthOfHand - 1) * this.cardSpacingIncrement));
+    return (window.innerWidth - (this.cardWidth + (lengthOfHand - 1) * this.cardSpacingIncrement)) / 2;
+  }
+
+  private resetAllCardPositions() {
+    for (let i = 0; i < this.cards.length; i++) {
+      const card = this.cards[i];
+      card.position.x = this.defaultCardPosition;
+    }
   }
 }
