cbe52e2c2f94590a1257f3dac4da5f19917eb895
Adam <adam.j.major@gmail.com>
2021-07-18 08:21:12 -0700

    Setup clouds for nebula animation

--- libs/replay-viewer/src/lib/components/nebula/nebula.component.ts
+++ libs/replay-viewer/src/lib/components/nebula/nebula.component.ts
@@ -1,10 +1,9 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import * as THREE from 'three';
-
 @Component({
   selector: 'nx-bridge-nebula',
   templateUrl: './nebula.component.html',
-  styleUrls: ['./nebula.component.scss']
+  styleUrls: ['./nebula.component.scss'],
 })
 export class NebulaComponent implements OnInit {
   private colorPrimary1 = 0x28537b;
@@ -13,44 +12,129 @@ export class NebulaComponent implements OnInit {
   private colorPrimary3 = 0xf4d262;
   private colorPrimary4 = 0xfbeeac;
   private colorRed = 0x7b3428;
-  private colorRedLight = 0xFF9494;
+  private colorRedLight = 0xff9494;
   private colorGreen = 0x537b28;
-  private colorGreenLight = 0x89C549;
+  private colorGreenLight = 0x89c549;
+  
   private canvasElement: HTMLCanvasElement | null | undefined = null;
-  private ambient = new THREE.AmbientLight(0x444444);
+  private insertionElementSelector = '#nebula';
+
   private renderer = new THREE.WebGLRenderer();
   private scene = new THREE.Scene();
-  private camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  constructor(
-    private elRef: ElementRef,
-  ) { }
+  private camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  private cameraZPositionStart = 1;
+  private cameraXRotationStart = 1.16;
+  private cameraYRotationStart = -0.12;
+  private cameraZRotationStart = 0.27;
+
+  private cloudOpacity = 0.85;
+  private cloudWidth = 500;
+  private numberOfCloudParticles = 50;
+  private cloudParticles: THREE.Mesh<
+    THREE.PlaneGeometry,
+    THREE.MeshLambertMaterial
+  >[] = [];
+ 
+  private cloudXRotationStart = 1.16;
+  private cloudYRotationStart = -0.12;
+  private cloudZRotationStart = Math.random() * 2 * Math.PI;
+  private cloudZRotationAmount = 0.001;
+
+  private fogColor = 0x03544e;
+  private fogDensity = 0.001;
+  private smokeImagePath = 'assets/replay-viewer/smoke.png';
+
+  private ambient = new THREE.AmbientLight(this.colorPrimary2);
+  // private directionalLight = new THREE.DirectionalLight(0xff8c19);
+
+  constructor(private elRef: ElementRef) {}
 
   ngOnInit(): void {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    const element = this.elRef.nativeElement as HTMLElement;
-    this.canvasElement = element.querySelector('#nebula')?.appendChild(this.renderer.domElement);
+    this.addCanvasToDom();
+    this.addEverythingToScene();
+    this.loadCloudParticles();
+    this.setCamera();
 
-    this.scene.add(this.ambient);
-    this.scene.fog = new THREE.FogExp2(this.colorPrimary4, 0.001);
-    this.renderer.setClearColor(this.scene.fog.color)
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-  
-      this.renderer.render(this.scene, this.camera);
-    };
-
-    this.camera.position.z = 1;
-    this.camera.rotation.x = 1.16;
-    this.camera.rotation.y = -.12;
-    this.camera.rotation.z = .27;
-
-
-    this.renderer.render(this.scene, this.camera);
-    animate();
+    this.startAnimation();
   }
 
-  
+  private addCanvasToDom() {
+    const element = this.elRef.nativeElement as HTMLElement;
+    this.canvasElement = element
+      .querySelector(this.insertionElementSelector)
+      ?.appendChild(this.renderer.domElement);
+  }
 
+  private addEverythingToScene() {
+    this.scene.add(this.ambient);
+
+    this.setRenderer();
+  }
+
+  private animateClouds() {
+    this.cloudParticles.forEach((cloud) => {
+      cloud.rotation.z -= this.cloudZRotationAmount;
+    });
+  }
+
+  private loadCloudParticles() {
+    const loader = new THREE.TextureLoader();
+    loader.load(this.smokeImagePath, (texture) => {
+      const cloudGeo = new THREE.PlaneBufferGeometry(
+        this.cloudWidth,
+        this.cloudWidth
+      );
+      const cloudMaterial = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true,
+        color: this.colorPrimary4,
+      });
+
+      for (let i = 0; i < this.numberOfCloudParticles; i++) {
+        const cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+        cloud.position.set(
+          Math.random() * 800 - 400,
+          500,
+          Math.random() * 500 - 500
+        );
+
+        cloud.rotation.x = this.cloudXRotationStart;
+        cloud.rotation.y = this.cloudYRotationStart;
+        cloud.rotation.z = this.cloudZRotationStart;
+        cloud.material.opacity = this.cloudOpacity;
+        this.scene.add(cloud);
+        this.cloudParticles.push(cloud);
+      }
+    });
+  }
+
+  private setCamera() {
+    this.camera.position.z = this.cameraZPositionStart;
+    this.camera.rotation.x = this.cameraXRotationStart;
+    this.camera.rotation.y = this.cameraYRotationStart;
+    this.camera.rotation.z = this.cameraZRotationStart;
+  }
+
+  private setRenderer() {
+    this.scene.fog = new THREE.FogExp2(this.fogColor, this.fogDensity);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(this.scene?.fog?.color);
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  private startAnimation() {
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      this.renderer.render(this.scene, this.camera);
+      this.animateClouds();
+    };
+    animate();
+  }
 }
