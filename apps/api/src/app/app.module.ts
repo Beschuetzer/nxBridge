bceb63f5f04afb860ab3f7262a7c7df490c60d1d
Adam <adam.j.major@gmail.com>
2021-07-20 20:55:36 -0700

    Add serve static package

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { ControllersModule } from './controllers/controllers.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { ServeStaticModule } from '@nestjs/serve-static'; // <- INSERT LINE
+import { join } from 'path'; // <- INSERT LINE
 
 @Module({
   imports: [
@@ -12,6 +14,10 @@ dotenv.config();
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.mongoDB_USER}:${process.env.mongoDB_PASSWORD}@cluster0.3trbv.mongodb.net/Bridge?retryWrites=true&w=majority`,
     ),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend'),
+      exclude: ['/api*']
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
